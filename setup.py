a929b209de83337764968b0e62d430318284c8ab
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2022-03-10 18:02:49 +0100

    Make newbie installable
    Adds setup.py and newbie/__init__.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'newbie',
+    version = '0.1.dev',
+    author = 'Benjamin Jung',
+    license = 'BSD-3-Clause',
+    packages = find_packages(include = ['newbie', 'newbie.*']),
+    description = 'A package for bayesian inference with nuclear waste.',
+)
