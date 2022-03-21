5e1a022f34a3a1276edb77b2ae1e841d3767d208
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2022-03-21 16:04:26 +0100

    Add WasteBinMixture class to wastebin
    The class is intended for treating mixtures of different
    waste batches. Currently, only mixtures of two batches work
    in a limited manner.

--- newbie/wastebin.py
+++ newbie/wastebin.py
@@ -12,6 +12,9 @@ import os
 import numpy as np
 import pymc3 as pm
 
+from itertools import chain
+
+
 class WasteBin():
     """Bayesian inference with nuclear waste
 
@@ -24,7 +27,7 @@ class WasteBin():
 
         Parameters
         ----------
-        model
+        model_type
             Surrogate model for predicting the value of an
             isotope concentration or ratio from the operating
             parameters.
@@ -86,17 +89,17 @@ class WasteBin():
         """
         for i in ids:
             self.models[i] = self.model_type.from_file(self.filepaths[i])
-            
+
     def _joint_probability(self, ids, dists):
         """Calculate the join probability distribution
-        
+
         Parameters
         ----------
         ids : list of str
             Identifiers of isotopes or isotopic ratios.
         dists : list
             Probability density of each surrogate model.
-            
+
         Returns
         l : pymc.DensityDist
             Joint probability density distribution of the
@@ -109,16 +112,16 @@ class WasteBin():
                 return np.product([n for i, n in kwargs.items()])
             l = pm.DensityDist('L', joint, observed=dict(zip(ids, dists)))
         return l
-            
+
     def inference(self, ids, limits, uncertainty=0.1, const=None,
                   plot=True, load=None, **kwargs):
         """Run bayesian inference with pymc uniform priors
-        
+
         Creates the Model context manager of pymc and runs bayesian
         inference with the specified parameters.
         Parameters that are also variable in the surrogate model, but
         are not intended for inference are set to constant values.
-        
+
         Parameters
         ----------
         ids : list of str
@@ -144,7 +147,7 @@ class WasteBin():
             loaded from the trace and the sampler is not run.
         **kwargs
             Keyword arguments for the pymc.sample method.
-        
+
         Returns
         -------
         summaries : dict
@@ -152,12 +155,12 @@ class WasteBin():
         trace : pymc trace object
             Trace of the sampling algorithm.
         """
-        
+
         with pm.Model():
             ## Needs to be called inside the context manager
             ## Otherwise models don't work
             self.load_models(ids)
-            
+
             labels = self.labels
             ## Create priors
             priors = []
@@ -166,7 +169,7 @@ class WasteBin():
                     priors.append(pm.Uniform(l, **limits[l]))
                 else:
                     priors.append(const[l])
-            
+
             evidence = [self.evidence[i] for i in ids]
             sigma = [uncertainty * e for e in evidence]
             models = [self.models[i].predict(priors) for i in ids]
@@ -174,7 +177,7 @@ class WasteBin():
                        for i, m, s, o in zip(ids, models, sigma, evidence)
                       ]
             self._joint_probability(ids, distrib)
-            
+
             if load is None:
                 ## Silence the deprecation warning
                 if 'return_inferencedata' not in kwargs:
@@ -182,9 +185,130 @@ class WasteBin():
                 trace = pm.sample(**kwargs)
             else:
                 trace = pm.load_trace(load)
-            
+
             if plot:
                 pm.plot_trace(trace, lines=list(limits.keys()))
-                
+
+            summaries = {n: pm.summary(trace, n).T for n in limits}
+            return summaries, trace
+
+
+class WasteBinMixture(WasteBin):
+    """Bayesian inference with a mixture of nuclear wastes.
+
+    The GPR models are trained on concentrations of single
+    isotope, but the inference is done with ratios. Thus
+    several GPR models are combined to predict one ratio.
+    """
+
+    def load_models(self, ids, submodel_type):
+        """Load the surrogate models from files
+
+        Parameters
+        ----------
+        ids : list
+            String identifiers of the isotopes or isotopic
+            ratios.
+        submodel_type
+            Type of kernel to be used for the composite
+            kernel.
+        """
+        for i in ids:
+            self.models[i] = self.model_type.from_file(self.filepaths[i],
+                                                       submodel_type,
+                                                       self.filepaths[i],
+                                                       submodel_type,
+                                                       )
+
+    def inference(self, ids, submodel_type, limits, uncertainty=0.1, const=None,
+                  plot=True, load=None, **kwargs):
+        """Run bayesian inference with pymc uniform priors
+
+        Creates the Model context manager of pymc and runs bayesian
+        inference with the specified parameters. Isotopic ratios
+        of the mixture are calculated by summing the GPR models of
+        the isotope ratios and dividing to obtain the ratios.
+        Parameters that are also variable in the surrogate model, but
+        are not intended for inference are set to constant values.
+
+        Currently only intended for a mixture of two batches with the
+        PredictorSum2 kernel.
+
+        Parameters
+        ----------
+        ids : list of str
+            Identifiers of isotopic ratios to be used in the inference.
+        limits : dict
+            Keys are the labels of the parameters that are to be
+            reconstructed. Each entry is a dict with keys 'lower'
+            and 'upper', specifying the lower and upper limits of
+            the uniform prior distribution of the parameter.
+        uncertainty : float, optional (default is 0.1)
+            Relative uncertainty assumed for the evidence.
+        const : dict of theano floats, optional (default is None)
+            Must be specified if not all elements of `reconstruct` are
+            True. Specifies the constant values of the parameters that
+            are not varied. Keys are parameter labels. All parameter
+            labels must be either in `limits` or `const`.
+        plot : bool, optional (default is True)
+            If True, plots the trace after running the sampler.
+        load : str, optional (default is None)
+            Path to a directory, where a trace was stored by a previous
+            sampling process. If the path is specified, the model is
+            loaded from the trace and the sampler is not run.
+        **kwargs
+            Keyword arguments for the pymc.sample method.
+
+        Returns
+        -------
+        summaries : dict
+            Key properties of the reconstructed distributions.
+        trace : pymc trace object
+            Trace of the sampling algorithm.
+        """
+
+        with pm.Model():
+            ## Needs to be called inside the context manager
+            ## Otherwise models don't work
+            iso_ids = list(set(list(
+                chain(*list(map(lambda x: x.split('/'), ids)))
+            )))
+            self.load_models(iso_ids, submodel_type)
+
+            labels = self.labels
+            ## Create priors
+            priors = []
+            for l in labels:
+                if l in limits:
+                    priors.append(pm.Uniform(l, **limits[l]))
+                else:
+                    priors.append(const[l])
+
+            evidence = [self.evidence[i] for i in ids]
+            sigma = [uncertainty * e for e in evidence]
+            models = []
+            for i in ids:
+                j, k = i.split('/')
+                models.append(self.models[j].predict(priors[0], priors[1:3],
+                                                     priors[3], priors[4:6])
+                            / self.models[k].predict(priors[0], priors[1:3],
+                                                     priors[3], priors[4:6])
+                            )
+            distrib = [pm.Normal(i, mu=m, sd=s, observed=o)
+                       for i, m, s, o in zip(ids, models, sigma, evidence)
+                      ]
+            self._joint_probability(ids, distrib)
+
+            if load is None:
+                ## Silence the deprecation warning
+                if 'return_inferencedata' not in kwargs:
+                    kwargs['return_inferencedata'] = False
+                trace = pm.sample(**kwargs)
+            else:
+                trace = pm.load_trace(load)
+
+            if plot:
+                pm.plot_trace(trace, lines=list(limits.keys()))
+
             summaries = {n: pm.summary(trace, n).T for n in limits}
             return summaries, trace
