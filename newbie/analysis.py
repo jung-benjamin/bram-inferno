d78fc1ad5523a0ca8507df68b238b5c391ff4ae5
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2023-04-04 11:30:01 +0200

    Fix bugs in analysis module
    The bug fix in the test file identified bugs in the analysis modle.
    These bugs are fixed here.

--- newbie/analysis.py
+++ newbie/analysis.py
@@ -6,15 +6,16 @@ and automatically assess a large number if inference results and
 evaluate the performance of the inference model.
 """
 
-import pandas as pd
 import arviz as az
+import pandas as pd
+import xarray as xr
 
 from newbie import metrics
 
 
 class PosteriorAnalysis:
 
-    def __init__(self, *args):
+    def __init__(self, *args, estimators=['peak', 'mean', 'mode']):
         """Instantiate the class with input arguments
         
         Parameters
@@ -22,12 +23,14 @@ class PosteriorAnalysis:
         """
         self.analyses = {}
         for i, d, t in args:
-            self.analyses[i] = metrics.MetricSet(d, t)
+            self.analyses[i] = metrics.MetricSet(d, t, estimators)
 
     @classmethod
-    def from_filepaths(cls, inference_files, truth_file, ids):
+    def from_filepaths(cls, inference_files, truth_file, ids=None):
         """Instantiate class with data stored in files."""
         truth = pd.read_csv(truth_file, index_col=0)
+        if ids:
+            ids = list(truth.index)
         idata = {}
         if isinstance(inference_files, dict):
             for i in ids:
@@ -38,19 +41,17 @@ class PosteriorAnalysis:
         elif isinstance(inference_files, list):
             for i in ids:
                 for f in inference_files:
-                    if i in f:
+                    if i in f.name:
                         idata[i] = az.from_json(f)
         tr = [xr.Dataset(truth.loc[n].to_dict()) for n in idata]
         args = list(zip(*zip(*idata.items()), tr))
         return cls(*args)
 
-    def calculate_distances(self,
-                            estimators=['peak', 'mean', 'mode'],
-                            **kwargs):
+    def calculate_distances(self, **kwargs):
         """Calculate distances between estimators and true parameters."""
         dist = []
         for n, it in self.analyses.items():
-            d = it.calculate_distances(estimators, **kwargs)
+            d = it.calculate_distances(**kwargs)
             dist.append(d)
         ds = xr.concat(dist, dim='ID')
         return ds.assign_coords(ID=list(self.analyses))
