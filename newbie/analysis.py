e7a7e1ed8e02f782b6281a2ffd510529b6d05ecf
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2023-04-04 17:14:22 +0200

    Add logging functionality to PosteriorAnalysis

--- newbie/analysis.py
+++ newbie/analysis.py
@@ -5,6 +5,7 @@ Contains functions to facilitate analyzing the Bayesian posteriors
 and automatically assess a large number if inference results and
 evaluate the performance of the inference model.
 """
+import logging
 
 import arviz as az
 import pandas as pd
@@ -15,7 +16,10 @@ from newbie import metrics
 
 class PosteriorAnalysis:
 
-    def __init__(self, *args, estimators=['peak', 'mean', 'mode']):
+    def __init__(self,
+                 *args,
+                 estimators=['peak', 'mean', 'mode'],
+                 loglevel='INFO'):
         """Instantiate the class with input arguments
         
         Parameters
@@ -24,13 +28,31 @@ class PosteriorAnalysis:
         self.analyses = {}
         for i, d, t in args:
             self.analyses[i] = metrics.MetricSet(d, t, estimators)
+        self.logger = self.get_logger(loglevel=loglevel)
 
     @classmethod
-    def from_filepaths(cls, inference_files, truth_file, ids=None):
+    def get_logger(cls, loglevel='INFO'):
+        """Configure a logger for the class."""
+        log = logging.getLogger(cls.__name__)
+        log.setLevel(getattr(logging, loglevel.upper()))
+        ch = logging.StreamHandler()
+        ch.setLevel(getattr(logging, loglevel.upper()))
+        log.addHandler(ch)
+        return log
+
+    @classmethod
+    def from_filepaths(cls,
+                       inference_files,
+                       truth_file,
+                       ids=None,
+                       loglevel='INFO'):
         """Instantiate class with data stored in files."""
+        log = cls.get_logger(loglevel)
         truth = pd.read_csv(truth_file, index_col=0)
-        if ids:
+        if not ids:
             ids = list(truth.index)
+        log.info('Creating PosteriorAnalysis...')
+        log.info(f'IDs: {ids}')
         idata = {}
         if isinstance(inference_files, dict):
             for i in ids:
@@ -38,6 +60,7 @@ class PosteriorAnalysis:
                     idata[i] = az.from_json(inference_files[i])
                 except KeyError:
                     msg = f'Warning: {i} has no corresponding inference file.'
+                    log.warning(msg)
         elif isinstance(inference_files, list):
             for i in ids:
                 for f in inference_files:
@@ -45,7 +68,7 @@ class PosteriorAnalysis:
                         idata[i] = az.from_json(f)
         tr = [xr.Dataset(truth.loc[n].to_dict()) for n in idata]
         args = list(zip(*zip(*idata.items()), tr))
-        return cls(*args)
+        return cls(*args, loglevel=loglevel)
 
     def calculate_distances(self, **kwargs):
         """Calculate distances between estimators and true parameters."""
