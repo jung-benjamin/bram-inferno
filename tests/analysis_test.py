1190f7d291fb4d9de34239bdb66a417b448b60a7
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2023-04-04 11:26:32 +0200

    Fix tests for analysis module
    The test file was incorrectly configured so that the tests were not
    carried out. This is remedied and bugs in the tests are fixed.

--- tests/analysis_test.py
+++ tests/analysis_test.py
@@ -20,33 +20,29 @@ def load_truth(rootdir, key):
     """Load the true parameter values to an xarray dataset."""
     df = pd.read_csv(Path(rootdir, 'test-data', 'synthetic_truth.csv'),
                      index_col=0)
-    df = df.loc[k].to_dict()
+    df = df.loc[key].to_dict()
     return xr.Dataset(df)
 
 
-def posterior_analysis_test(rootdir):
+def test_posterior_analysis(rootdir):
     """Test for the posterior analysis class."""
     keys = ['data', 'data_2']
     idata = [
         load_idata(rootdir, 'inference_data.json'),
         load_idata(rootdir, 'inference_data_2.json')
     ]
-    truth = list(map(lambda x: load_truth(rootdir, x)), keys)
+    truth = list(map(lambda x: load_truth(rootdir, x), keys))
     pa = analysis.PosteriorAnalysis((keys[0], idata[0], truth[0]),
                                     (keys[1], idata[1], truth[1]))
-    pa.calculate_distances()
-    assert True
+    assert pa.calculate_distances()
 
 
-def posterior_analysis_from_files_test(rootdir):
+def test_posterior_analysis_from_files(rootdir):
     """Test for the from_filepaths classmethod of PosteriorAnalysis."""
     keys = ['data', 'data_2']
-    truth = Path(rootdir, 'tests', 'test-data', 'synthetic_truth.csv')
+    truth = Path(rootdir, 'test-data', 'synthetic_truth.csv')
     idata = {
-        'data':
-        Path(rootdir, 'tests', 'test-data', 'inference_data.json'),
-        'data_2':
-        Path(rootdir, 'tests', 'test-data', 'inference_data_2.json')
+        'data': Path(rootdir, 'test-data', 'inference_data.json'),
+        'data_2': Path(rootdir, 'test-data', 'inference_data_2.json')
     }
-    pa = analysis.PosteriorAnalysis.from_filepaths(idata, truth, keys)
-    assert True
+    assert analysis.PosteriorAnalysis.from_filepaths(idata, truth, keys)
