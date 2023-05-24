9c31e77ba76964864b75439b596606507ca8394b
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2023-05-24 17:38:31 +0200

    Make inference_data optional in estimator init
    The inference data can optionally be passed as an argument to the
    calculate_estimator method after initialization.

--- tests/estimator_test.py
+++ tests/estimator_test.py
@@ -17,28 +17,37 @@ def load_idata(rootdir):
 def test_mode_estimator(rootdir):
     """Test the mode estimator"""
     data = load_idata(rootdir)
-    me = estimators.EstimatorFactory.create_estimator('mode', data)
-    mode = me.calculate_estimator()
-    assert True
-    mode2 = me.calculate_estimator('burnupA')
-    assert True
+    e1 = estimators.EstimatorFactory.create_estimator('mode', data)
+    m1 = e1.calculate_estimator()
+    m1A = e1.calculate_estimator('burnupA')
+    e2 = estimators.EstimatorFactory.create_estimator('mode')
+    m2 = e2.calculate_estimator(inference_data=data)
+    m2A = e2.calculate_estimator('burnupA', inference_data=data)
+    assert m1 == m2
+    assert m1A == m2A
 
 
 def test_mean_estimator(rootdir):
     """Test the mean estimator"""
     data = load_idata(rootdir)
-    me = estimators.EstimatorFactory.create_estimator('mean', data)
-    mean = me.calculate_estimator()
-    assert True
-    mean2 = me.calculate_estimator('burnupA')
-    assert True
+    e1 = estimators.EstimatorFactory.create_estimator('mean', data)
+    m1 = e1.calculate_estimator()
+    m1A = e1.calculate_estimator('burnupA')
+    e2 = estimators.EstimatorFactory.create_estimator('mean')
+    m2 = e2.calculate_estimator(inference_data=data)
+    m2A = e2.calculate_estimator('burnupA', inference_data=data)
+    assert m1 == m2
+    assert m1A == m2A
 
 
 def test_peak_estimator(rootdir):
     """Test the peak estimator"""
     data = load_idata(rootdir)
-    me = estimators.EstimatorFactory.create_estimator('peak', data)
-    peak = me.calculate_estimator()
-    assert True
-    peak2 = me.calculate_estimator('burnupA')
-    assert True
+    e1 = estimators.EstimatorFactory.create_estimator('peak', data)
+    m1 = e1.calculate_estimator()
+    m1A = e1.calculate_estimator('burnupA')
+    e2 = estimators.EstimatorFactory.create_estimator('peak')
+    m2 = e2.calculate_estimator(inference_data=data)
+    m2A = e2.calculate_estimator('burnupA', inference_data=data)
+    assert m1 == m2
+    assert m1A == m2A
