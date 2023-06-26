f3e2b0b7309b2e1df25f989c8f9ee23fefdb1050
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2023-06-26 18:05:03 +0200

    Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'newbie',
-    version = '0.6.4',
+    version = '0.6.5',
     author = 'Benjamin Jung',
     license = 'BSD-3-Clause',
     packages = find_packages(include = ['newbie', 'newbie.*']),
