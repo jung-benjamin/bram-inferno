9791c56aa24fac5d1c53f304da56ff3212f014c6
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2024-12-17 11:19:27 +0100

    Change package specifications to pyproject.toml
    Installing now requires pymc>=5, although the code should still work
    with pymc3.

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(
-    name = 'newbie',
-    version = '0.7.1',
-    author = 'Benjamin Jung',
-    license = 'BSD-3-Clause',
-    packages = find_packages(include = ['newbie', 'newbie.*']),
-    description = 'A package for bayesian inference with nuclear waste.',
-    entry_points={
-        'console_scripts': [
-            'evaluate_posteriors = scripts:evaluate_posteriors'
-            ]
-        },
-)
+setup()
